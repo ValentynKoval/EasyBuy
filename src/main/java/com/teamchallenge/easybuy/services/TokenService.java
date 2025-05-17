2034be135e2aa6f97de12ca14ac73b338d899dd6
Valentyn <J.K.play2015@gmail.com>
2025-05-17 16:43:18 +0300

    create token service

---
+++ src/main/java/com/teamchallenge/easybuy/services/TokenService.java
@@ -0,0 +1,45 @@
+package com.teamchallenge.easybuy.services;
+
+import com.teamchallenge.easybuy.models.Token;
+import com.teamchallenge.easybuy.models.User;
+import com.teamchallenge.easybuy.repo.TokenRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+
+@Service
+@RequiredArgsConstructor
+public class TokenService {
+    private final TokenRepository tokenRepository;
+
+    @Value( "${jwt.refreshTokenExpiration}")
+    private long refreshTokenDurationMs;
+
+    public void createToken(User user, String refreshToken) {
+        tokenRepository.save(
+                Token.builder()
+                        .user(user)
+                        .token(refreshToken)
+                        .expiryDate(Instant.now().plusMillis(refreshTokenDurationMs))
+                        .revoked(false)
+                        .build()
+        );
+    }
+
+    public boolean isValid(Token token) {
+        return !token.isRevoked() && token.getExpiryDate().isAfter(Instant.now());
+    }
+
+    public void revokeToken(Token token) {
+        token.setRevoked(true);
+        tokenRepository.save(token);
+    }
+
+    @Transactional
+    public void revokeAllTokensTorUser(User user) {
+        tokenRepository.deleteAllByUser(user);
+    }
+}
