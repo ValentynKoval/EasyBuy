88cce9346f297cf2b40e8c8193fe3214e27ac296
Valentyn <J.K.play2015@gmail.com>
2025-05-17 12:29:00 +0300

    create authentication service class

---
+++ src/main/java/com/teamchallenge/easybuy/services/AuthenticationService.java
@@ -0,0 +1,67 @@
+package com.teamchallenge.easybuy.services;
+
+import com.teamchallenge.easybuy.dto.AuthResponse;
+import com.teamchallenge.easybuy.dto.LoginRequest;
+import com.teamchallenge.easybuy.dto.RegisterRequest;
+import com.teamchallenge.easybuy.models.Role;
+import com.teamchallenge.easybuy.models.User;
+import com.teamchallenge.easybuy.repo.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+/**
+ * Service for handling user registration and authentication.
+ * Hashes passwords, authenticates users via AuthenticationManager,
+ * and generates access and refresh tokens for authenticated sessions.
+ */
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthResponse register(RegisterRequest request) {
+        if (userRepository.existsByEmail(request.getEmail())) {
+            throw new RuntimeException("Email already exists!");
+        }
+
+        User user = new User();
+        user.setEmail(request.getEmail());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setPhoneNumber(request.getPhoneNumber());
+        user.setCreatedAt(LocalDateTime.now());
+        user.setEmailVerified(false);
+        user.setRole(Role.CUSTOMER);
+        userRepository.save(user);
+
+        String accessToken = jwtService.generateAccessToken(user.getEmail(), user.getRole().name());
+        String refreshToken = jwtService.generateRefreshToken(user.getEmail(), user.getRole().name());
+
+        return new AuthResponse(accessToken, refreshToken);
+    }
+
+    public AuthResponse authenticate(LoginRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(), request.getPassword()
+                )
+        );
+
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new RuntimeException("User not found with email : " + request.getEmail()));
+
+        String accessToken = jwtService.generateAccessToken(user.getEmail(), user.getRole().name());
+        String refreshToken = jwtService.generateRefreshToken(user.getEmail(), user.getRole().name());
+
+        return new AuthResponse(accessToken, refreshToken);
+    }
+}
