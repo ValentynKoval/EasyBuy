6e40832dd9d46659a9ffda60083ae78ab76f5a24
Valentyn <J.K.play2015@gmail.com>
2025-05-17 19:04:22 +0300

    change name of method

--- src/main/java/com/teamchallenge/easybuy/dto/LoginRequestDto.java
+++ src/main/java/com/teamchallenge/easybuy/dto/LoginRequestDto.java
@@ -3,7 +3,7 @@ package com.teamchallenge.easybuy.dto;
 import lombok.Data;
 
 @Data
-public class LoginRequest {
+public class LoginRequestDto {
     private String email;
     private String password;
 }
--- src/main/java/com/teamchallenge/easybuy/dto/RegisterRequestDto.java
+++ src/main/java/com/teamchallenge/easybuy/dto/RegisterRequestDto.java
@@ -3,7 +3,7 @@ package com.teamchallenge.easybuy.dto;
 import lombok.Data;
 
 @Data
-public class RegisterRequest {
+public class RegisterRequestDto {
     private String email;
     private String password;
     private String firstName;
