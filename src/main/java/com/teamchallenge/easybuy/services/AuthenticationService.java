b71f46276a843b4b15c3790e73a9459ef81351c6
Valentyn <J.K.play2015@gmail.com>
2025-05-18 16:19:53 +0300

    add refresh and logout methods

--- src/main/java/com/teamchallenge/easybuy/services/AuthenticationService.java
+++ src/main/java/com/teamchallenge/easybuy/services/AuthenticationService.java
@@ -3,13 +3,16 @@ package com.teamchallenge.easybuy.services;
 import com.teamchallenge.easybuy.dto.AuthResponseDto;
 import com.teamchallenge.easybuy.dto.LoginRequestDto;
 import com.teamchallenge.easybuy.dto.RegisterRequestDto;
-import com.teamchallenge.easybuy.exceptions.UserAlreadyExistsException;
 import com.teamchallenge.easybuy.models.Role;
+import com.teamchallenge.easybuy.models.Token;
 import com.teamchallenge.easybuy.models.User;
 import com.teamchallenge.easybuy.repo.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -29,9 +32,9 @@ public class AuthenticationService {
     private final AuthenticationManager authenticationManager;
     private final TokenService tokenService;
 
-    public void register(RegisterRequestDto request) {
+    public boolean register(RegisterRequestDto request) {
         if (userRepository.existsByEmail(request.getEmail())) {
-            throw new UserAlreadyExistsException(request.getEmail());
+            return false;
         }
 
         userRepository.save(User.builder()
@@ -42,24 +45,51 @@ public class AuthenticationService {
                 .phoneNumber(request.getPhoneNumber())
                 .createdAt(LocalDateTime.now())
                 .isEmailVerified(false)
-                .role(Role.CUSTOMER)
+                .role(Role.valueOf(request.getRole()))
                 .build());
+        return true;
     }
 
     public AuthResponseDto authenticate(LoginRequestDto request) {
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        request.getEmail(), request.getPassword()
-                )
-        );
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            request.getEmail(), request.getPassword()
+                    )
+            );
+            User user = userRepository.findByEmail(request.getEmail())
+                    .orElseThrow(() -> new RuntimeException("User not found with email : " + request.getEmail()));
 
-        User user = userRepository.findByEmail(request.getEmail())
-                .orElseThrow(() -> new RuntimeException("User not found with email : " + request.getEmail()));
+            String accessToken = jwtService.generateAccessToken(user.getEmail(), user.getRole().name());
+            String refreshToken = jwtService.generateRefreshToken(user.getEmail(), user.getRole().name());
+            tokenService.createToken(user, refreshToken);
 
+            return new AuthResponseDto(accessToken, refreshToken);
+        } catch (BadCredentialsException ex) {
+            return null;
+        }
+    }
+
+    public AuthResponseDto refresh(String refreshToken) {
+        Token token = tokenService.findByToken(refreshToken);
+        if (token == null || !tokenService.isValid(token)) {
+            return null;
+        }
+        User user = token.getUser();
         String accessToken = jwtService.generateAccessToken(user.getEmail(), user.getRole().name());
-        String refreshToken = jwtService.generateRefreshToken(user.getEmail(), user.getRole().name());
-        tokenService.createToken(user, refreshToken);
-
         return new AuthResponseDto(accessToken, refreshToken);
+    }
+
+    public void logout() {
+        String username = SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getName();
+
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found: " + username));
+
+        tokenService.revokeAllTokensForUser(user);
     }
 }
