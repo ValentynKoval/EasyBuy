612304aa636ffb61c025964720dc141d07ea3238
Valentyn <J.K.play2015@gmail.com>
2025-05-18 16:24:33 +0300

    adding a method to find all tokens by user and revoked false

--- src/main/java/com/teamchallenge/easybuy/repo/TokenRepository.java
+++ src/main/java/com/teamchallenge/easybuy/repo/TokenRepository.java
@@ -5,6 +5,7 @@ import com.teamchallenge.easybuy.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,4 +13,6 @@ public interface TokenRepository extends JpaRepository<Token, Integer> {
     Optional<Token> findByToken(String token);
 
     void deleteAllByUser(User user);
+
+    List<Token> findAllByUserAndRevokedFalse(User user);
 }
