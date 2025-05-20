16a3893f1de96cf154236a8a285b543a22aaf146
Valentyn <J.K.play2015@gmail.com>
2025-05-20 20:20:20 +0300

    adding confirm email end resend confirmation methods

--- src/main/java/com/teamchallenge/easybuy/controllers/AuthController.java
+++ src/main/java/com/teamchallenge/easybuy/controllers/AuthController.java
@@ -4,14 +4,16 @@ import com.teamchallenge.easybuy.dto.AuthResponseDto;
 import com.teamchallenge.easybuy.dto.LoginRequestDto;
 import com.teamchallenge.easybuy.dto.RefreshTokenRequestDto;
 import com.teamchallenge.easybuy.dto.RegisterRequestDto;
+import com.teamchallenge.easybuy.models.User;
 import com.teamchallenge.easybuy.services.AuthenticationService;
-import com.teamchallenge.easybuy.services.JwtService;
-import com.teamchallenge.easybuy.services.TokenService;
+import com.teamchallenge.easybuy.services.EmailConfirmationService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.Map;
 
@@ -21,10 +23,18 @@ import java.util.Map;
 public class AuthController {
 
     private final AuthenticationService authenticationService;
+    private final EmailConfirmationService emailConfirmationService;
 
     @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody RegisterRequestDto request) {
-        if(authenticationService.register(request)) {
+    public ResponseEntity<?> register(@RequestBody RegisterRequestDto registerRequestDto, HttpServletRequest request) {
+        String baseUrl = ServletUriComponentsBuilder
+                .fromRequestUri(request)
+                .replacePath(null)
+                .build()
+                .toUriString();
+        User user = authenticationService.register(registerRequestDto);
+        if(user != null) {
+            emailConfirmationService.sendConfirmationEmail(user, baseUrl);
             return ResponseEntity.status(HttpStatus.CREATED).build();
         }
         return ResponseEntity
@@ -32,15 +42,29 @@ public class AuthController {
                 .body(Map.of("error", "The user with this email is already registered"));
     }
 
+    @GetMapping("/confirm")
+    public ResponseEntity<?> confirm(@RequestParam("token") String token) {
+        try {
+            return ResponseEntity.ok(emailConfirmationService.confirmEmail(token));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.GONE).body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/resend-confirmation")
+    public ResponseEntity<?> resendConfirmation(@RequestParam String email, HttpServletRequest request) {
+        String baseUrl = ServletUriComponentsBuilder
+                .fromRequestUri(request)
+                .replacePath(null)
+                .build()
+                .toUriString();
+        emailConfirmationService.resendConfirmationEmail(email, baseUrl);
+        return ResponseEntity.ok("The new token has been sent to the email");
+    }
+
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequestDto request) {
-        AuthResponseDto authResponseDto = authenticationService.authenticate(request);
-        if (authResponseDto == null) {
-            return ResponseEntity
-                    .status(HttpStatus.UNAUTHORIZED)
-                    .body(Map.of("error", "Incorrect login or password"));
-        }
-        return ResponseEntity.ok(authResponseDto);
+        return authenticationService.authenticate(request);
     }
 
     @PostMapping("/refresh")
