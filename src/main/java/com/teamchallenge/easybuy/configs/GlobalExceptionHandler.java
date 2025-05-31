bf3a3492a18f6e60b195bc56277157c17ff193b3
Valentyn <J.K.play2015@gmail.com>
2025-05-31 19:16:31 +0300

    create global exception handler to send validation errors

---
+++ src/main/java/com/teamchallenge/easybuy/configs/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.teamchallenge.easybuy.configs;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Hidden
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            String field = error.getField();
+            String message = error.getDefaultMessage(); // те саме, що ти писав у DTO
+            errors.put(field, message);
+        });
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+}
