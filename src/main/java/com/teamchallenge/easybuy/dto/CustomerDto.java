43e61d3f150e4915bc9dfcb0538297977ce23cae
Valentyn <J.K.play2015@gmail.com>
2025-06-02 19:11:47 +0300

    create customer dto

---
+++ src/main/java/com/teamchallenge/easybuy/dto/CustomerDto.java
@@ -0,0 +1,20 @@
+package com.teamchallenge.easybuy.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CustomerDto {
+    private String email;
+    private String phoneNumber;
+    private String name;
+    private LocalDateTime birthday;
+    private AddressDto address;
+}
