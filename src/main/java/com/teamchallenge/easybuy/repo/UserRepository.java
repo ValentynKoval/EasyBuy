4c1ee4016f5b81eae11c3a694c2e75e78383f78e
Valentyn <J.K.play2015@gmail.com>
2025-06-02 14:58:53 +0300

    some changes method of user repository

--- src/main/java/com/teamchallenge/easybuy/repo/UserRepository.java
+++ src/main/java/com/teamchallenge/easybuy/repo/UserRepository.java
@@ -5,10 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
 
-    boolean existsByEmail(String email);
+    boolean existsByEmailAndPhoneNumber(String email, String phoneNumber);
 }
