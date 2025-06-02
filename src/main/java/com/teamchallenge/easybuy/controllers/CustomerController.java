30d57a96bc12346f4eb565d2fa53bcd965341754
Valentyn <J.K.play2015@gmail.com>
2025-06-02 19:11:32 +0300

    create a controller with endpoints to get, update and delete a user

---
+++ src/main/java/com/teamchallenge/easybuy/controllers/CustomerController.java
@@ -0,0 +1,33 @@
+package com.teamchallenge.easybuy.controllers;
+
+import com.teamchallenge.easybuy.dto.CustomerDto;
+import com.teamchallenge.easybuy.services.CustomerService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/customer")
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    @GetMapping("/profile")
+    public ResponseEntity<?> getProfile() {
+        return ResponseEntity.ok(customerService.getUserInfo());
+    }
+
+    @PutMapping("/profile")
+    public ResponseEntity<CustomerDto> updateProfile(@Valid @RequestBody CustomerDto customerDto) {
+        return ResponseEntity.status(HttpStatus.OK).body(customerService.updateProfile(customerDto));
+    }
+
+    @DeleteMapping("/profile")
+    public ResponseEntity<?> deleteProfile() {
+        customerService.deleteProfile();
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+}
