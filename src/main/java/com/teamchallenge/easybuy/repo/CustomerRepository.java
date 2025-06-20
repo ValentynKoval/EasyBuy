fe14f819189c8b992694518e8ab4ff27273fb730
Valentyn <J.K.play2015@gmail.com>
2025-06-20 16:29:02 +0300

    create customer repository

---
+++ src/main/java/com/teamchallenge/easybuy/repo/CustomerRepository.java
@@ -0,0 +1,13 @@
+package com.teamchallenge.easybuy.repo;
+
+import com.teamchallenge.easybuy.models.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, UUID> {
+    Optional<Customer> findByEmail(String email);
+}
