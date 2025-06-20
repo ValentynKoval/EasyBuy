fcdf1f567969ed459d9c8dcf63dde19e56cb760d
Valentyn <J.K.play2015@gmail.com>
2025-06-20 16:31:11 +0300

    change the path

--- src/main/java/com/teamchallenge/easybuy/repo/user/EmailConfirmationTokenRepository.java
+++ src/main/java/com/teamchallenge/easybuy/repo/user/EmailConfirmationTokenRepository.java
@@ -1,7 +1,7 @@
-package com.teamchallenge.easybuy.repo;
+package com.teamchallenge.easybuy.repo.user;
 
-import com.teamchallenge.easybuy.models.EmailConfirmationToken;
-import com.teamchallenge.easybuy.models.User;
+import com.teamchallenge.easybuy.models.user.EmailConfirmationToken;
+import com.teamchallenge.easybuy.models.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
