959c1e51feec6493ec72efe513bb37d8abdc2b21
Valentyn <J.K.play2015@gmail.com>
2025-06-27 13:43:56 +0300

    method for deleting all tokens by expired date

--- src/main/java/com/teamchallenge/easybuy/repo/user/EmailConfirmationTokenRepository.java
+++ src/main/java/com/teamchallenge/easybuy/repo/user/EmailConfirmationTokenRepository.java
@@ -4,10 +4,13 @@ import com.teamchallenge.easybuy.models.user.EmailConfirmationToken;
 import com.teamchallenge.easybuy.models.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface EmailConfirmationTokenRepository extends JpaRepository<EmailConfirmationToken, Integer> {
     Optional<EmailConfirmationToken> findByToken(String token);
 
     void deleteAllByUser(User user);
+
+    void deleteAllByExpiresAtBefore(LocalDateTime now);
 }
