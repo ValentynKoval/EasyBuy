2a911bf4c347a5779bfc7337956116e3cdae6c4c
valentyn <J.K.play2015@gmail.com>
2025-06-28 18:21:51 +0300

    add some annotations

--- src/main/java/com/teamchallenge/easybuy/models/goods/Shop.java
+++ src/main/java/com/teamchallenge/easybuy/models/goods/Shop.java
@@ -1,5 +1,18 @@
 package com.teamchallenge.easybuy.models.goods;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.util.UUID;
+
 @Entity
 public class Shop {
+    @Id
+    @GeneratedValue
+    @Column(name = "id", nullable = false, updatable = false)
+    @Schema(description = "Unique identifier for the shop", example = "a12c56d8-bf7f-4a12-b8ea-2d5d7f4db4a1")
+    private UUID id;
 }
