5e0553226fa99b7309332688bbd5526a638fa333
Alexander <alexandermagichi@gmail.com>
2026-03-06 02:52:42 +0200

    feat / add existsByShopName method and EntityGraph to ShopRepository for enhanced

--- src/main/java/com/teamchallenge/easybuy/repository/shop/ShopRepository.java
+++ src/main/java/com/teamchallenge/easybuy/repository/shop/ShopRepository.java
@@ -2,12 +2,20 @@ package com.teamchallenge.easybuy.repository.shop;
 
 
 import com.teamchallenge.easybuy.domain.model.shop.Shop;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.UUID;
 
 public interface ShopRepository extends JpaRepository<Shop, UUID>, JpaSpecificationExecutor<Shop> {
+    boolean existsByShopName(String shopName);
+
+    @Override
+    @EntityGraph(attributePaths = {"seller", "moderatedByUser"})
+    Page<Shop> findAll(Specification<Shop> spec, Pageable pageable);
 
 }
