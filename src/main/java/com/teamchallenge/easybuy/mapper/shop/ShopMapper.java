1d6a0e08495f1413872a904c7f4e495fbb905380
Alexander <alexandermagichi@gmail.com>
2026-03-06 16:04:39 +0200

    feat / enhance ShopMapper with updateEntityFromDto method and improve null handling

--- src/main/java/com/teamchallenge/easybuy/mapper/shop/ShopMapper.java
+++ src/main/java/com/teamchallenge/easybuy/mapper/shop/ShopMapper.java
@@ -2,25 +2,19 @@ package com.teamchallenge.easybuy.mapper.shop;
 
 import com.teamchallenge.easybuy.domain.model.shop.Shop;
 import com.teamchallenge.easybuy.dto.shop.ShopDTO;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.ReportingPolicy;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ShopMapper {
 
     @Mapping(source = "seller.id", target = "sellerId")
-
-    // TODO: When moderation logic is implemented,
-    // map moderatedByUser.id to moderatedByUserId for response DTO
     @Mapping(source = "moderatedByUser.id", target = "moderatedByUserId")
-
     ShopDTO toDto(Shop shop);
 
-    @Mapping(source = "sellerId", target = "seller.id")
-
-        // IMPORTANT:
-        // Do NOT map moderatedByUserId back to entity.
-        // This field must be set only by moderation service logic.
+    // IMPORTANT:
+    // Relationships are set manually in service layer
     Shop toEntity(ShopDTO shopDTO);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateEntityFromDto(ShopDTO dto, @MappingTarget Shop entity);
 }
