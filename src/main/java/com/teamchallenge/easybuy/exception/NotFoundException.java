bc699a435b1595ded27d1a47d7820d2d92d78fa8
Alexander <alexandermagichi@gmail.com>
2026-03-08 01:41:02 +0200

    feat / add NotFoundException for handling resource not found scenarios

---
+++ src/main/java/com/teamchallenge/easybuy/exception/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.teamchallenge.easybuy.exception;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
