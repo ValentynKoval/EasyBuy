c74806acf5d4672f3331fe36d1ea41aa34b53af2
Alexander <alexandermagichi@gmail.com>
2026-03-08 01:41:31 +0200

    feat / add ShopNotFoundException for handling missing shop scenarios

---
+++ src/main/java/com/teamchallenge/easybuy/exception/Shop/ShopNotFoundException.java
@@ -0,0 +1,8 @@
+package com.teamchallenge.easybuy.exception.Shop;
+
+public class ShopNotFoundException extends RuntimeException {
+
+    public ShopNotFoundException(String message) {
+        super(message);
+    }
+}
