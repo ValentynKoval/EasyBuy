c175123e244fc3b9f2c7845e70c7a4e2b2fabf0a
Alexander <alexandermagichi@gmail.com>
2026-04-04 22:53:15 +0300

    refactor / update CustomerMapper to use `disableBuilder` in @Mapper configuration and adjust package structure

--- src/main/java/com/teamchallenge/easybuy/mapper/user/CustomerMapper.java
+++ src/main/java/com/teamchallenge/easybuy/mapper/user/CustomerMapper.java
@@ -1,11 +1,15 @@
-package com.teamchallenge.easybuy.mapper;
+package com.teamchallenge.easybuy.mapper.user;
 
 import com.teamchallenge.easybuy.dto.user.CustomerProfileDto;
 import com.teamchallenge.easybuy.domain.model.user.Customer;
+import org.mapstruct.Builder; // Не забудь импортировать Builder из MapStruct!
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", builder = @Builder(disableBuilder = true))
 public interface CustomerMapper {
+
+
     CustomerProfileDto toDto(Customer customer);
 
     Customer toEntity(CustomerProfileDto dto);
