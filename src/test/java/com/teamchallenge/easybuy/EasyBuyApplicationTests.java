a6b97c8ec931a0439106dd0c4018c10a49dd1e73
Alexander <alexandermagichi@gmail.com>
2026-04-04 22:58:18 +0300

    test / enhance EasyBuyApplicationTests to verify ApplicationContext initialization using `@IntegrationTest` annotation

--- src/test/java/com/teamchallenge/easybuy/EasyBuyApplicationTests.java
+++ src/test/java/com/teamchallenge/easybuy/EasyBuyApplicationTests.java
@@ -1,13 +1,20 @@
 package com.teamchallenge.easybuy;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@IntegrationTest
 class EasyBuyApplicationTests {
+
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @Test
     void contextLoads() {
+        assertNotNull(applicationContext);
     }
 
 }
