0f7bcebccc582d65d58b9c72fae8a10cb13dfa29
Alexander <alexandermagichi@gmail.com>
2026-04-04 22:54:34 +0300

    feat / extend CategoryMapper to support Goods and Shop mapping with utility methods for UUID conversion

--- src/main/java/com/teamchallenge/easybuy/mapper/goods/category/CategoryMapper.java
+++ src/main/java/com/teamchallenge/easybuy/mapper/goods/category/CategoryMapper.java
@@ -1,10 +1,15 @@
 package com.teamchallenge.easybuy.mapper.goods.category;
 
-import com.teamchallenge.easybuy.dto.goods.category.CategoryDTO;
+import com.teamchallenge.easybuy.domain.model.goods.Goods;
 import com.teamchallenge.easybuy.domain.model.goods.category.Category;
+import com.teamchallenge.easybuy.domain.model.shop.Shop;
+import com.teamchallenge.easybuy.dto.goods.GoodsDTO;
+import com.teamchallenge.easybuy.dto.goods.category.CategoryDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
+
+import java.util.UUID;
 
 @Mapper(componentModel = "spring", uses = CategoryAttributeMapper.class, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface CategoryMapper {
@@ -20,4 +25,24 @@ public interface CategoryMapper {
     @Mapping(target = "subcategories", ignore = true)
     @Mapping(target = "attributes", ignore = true)
     Category toEntity(CategoryDTO categoryDTO);
+
+    @Mapping(source = "shop.shopId", target = "shopId")
+    GoodsDTO toDto(Goods goods);
+
+    @Mapping(source = "shopId", target = "shop.shopId")
+    Goods toEntity(GoodsDTO dto);
+
+    // Shop -> UUID
+    default UUID map(Shop shop) {
+        if (shop == null) return null;
+        return shop.getShopId();
+    }
+
+    //  UUID -> Shop
+    default Shop map(UUID id) {
+        if (id == null) return null;
+        Shop shop = new Shop();
+        shop.setShopId(id);
+        return shop;
+    }
 }
