535bd06bc9d2d4556a3036f3e156b30912aec3d4
Alexander <alexandermagichi@gmail.com>
2026-04-09 02:58:04 +0300

    test / add MockitoBean for RedisConnectionFactory in integration tests

--- src/test/java/com/teamchallenge/easybuy/EasyBuyApplicationTests.java
+++ src/test/java/com/teamchallenge/easybuy/EasyBuyApplicationTests.java
@@ -3,14 +3,17 @@ package com.teamchallenge.easybuy;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @IntegrationTest
+@MockitoBean(types = org.springframework.data.redis.connection.RedisConnectionFactory.class)
 class EasyBuyApplicationTests {
 
     @Autowired
     private ApplicationContext applicationContext;
+
 
     @Test
     void contextLoads() {
