560978dd8a30698f0440e90410860b2da0935baf
Alexander <alexandermagichi@gmail.com>
2026-04-09 23:14:51 +0300

    build / remove unused NotNull import from SellerRepository

--- src/main/java/com/teamchallenge/easybuy/repository/user/seller/SellerRepository.java
+++ src/main/java/com/teamchallenge/easybuy/repository/user/seller/SellerRepository.java
@@ -1,7 +1,6 @@
 package com.teamchallenge.easybuy.repository.user.seller;
 
 import com.teamchallenge.easybuy.domain.model.user.Seller;
-import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
