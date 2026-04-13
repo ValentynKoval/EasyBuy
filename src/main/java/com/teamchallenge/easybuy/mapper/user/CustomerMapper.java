d9b2126430b89f8cbb7f7b86726a43476fe79d2e
Alexander <alexandermagichi@gmail.com>
2026-04-13 19:00:49 +0300

    build / update CustomerMapper to remove unused imports and streamline code

--- src/main/java/com/teamchallenge/easybuy/mapper/user/CustomerMapper.java
+++ src/main/java/com/teamchallenge/easybuy/mapper/user/CustomerMapper.java
@@ -2,9 +2,8 @@ package com.teamchallenge.easybuy.mapper.user;
 
 import com.teamchallenge.easybuy.dto.user.CustomerProfileDto;
 import com.teamchallenge.easybuy.domain.model.user.Customer;
-import org.mapstruct.Builder; // Не забудь импортировать Builder из MapStruct!
+import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring", builder = @Builder(disableBuilder = true))
 public interface CustomerMapper {
